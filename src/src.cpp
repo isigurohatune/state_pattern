b279904cc36ee5ec980951fffda52d34833d9f2c
isigurohatune <39hatune.1925@gmail.com>
2021-05-24 16:09:05 +0900

    ステートパターンで書きなおしました

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,44 @@
 #include <iostream>
 #include <thread>
+#include <stdio.h>
 
+class Scenestate
+{
+    public:
+        virtual ~Scenestate(){}
+        virtual void sceneChange(){}
+};
+
+class SceneChange :public Scenestate
+{
+    virtual void sceneChange()
+    {
+        
+   }
+};
 int main()
 {
-repeat:
-    std::cout << "credit\n";
+    int i;
+    for (i = 0; i < 16; i++)
+    {
+        std::cout << "credit\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+        std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
 
-    std::cout << "title\n";
+        std::cout << "title\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+        std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
 
-    std::cout << "opening\n";
+        std::cout << "opening\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+        std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
 
-    std::cout << "demo\n";
+        std::cout << "demo\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-    
-    goto repeat;
+        std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+
+        continue;
+    }
 }
+
 
